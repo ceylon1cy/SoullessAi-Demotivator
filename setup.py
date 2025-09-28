c0022fde81166f81e32ae0ffdbd36de649471a29
ceylon1cy <abb148852@gmail.com>
2025-09-29 02:09:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="SoullessAi-Dem",
-    version="1.0.0",
+    version="1.0.4",
     author="ceylon",
     author_email="ceylon1cy@gmail.com",
     description="Библиотека для генерации демотиваторов",
